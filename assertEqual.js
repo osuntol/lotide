2c7038432b777edb8db5a721589354c171f1b237
Rotimi Osuntola Jr <rosuntola@gmail.com>
2025-01-28 20:09:38 -0500

    Implement Head and used in assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,7 @@
+
+
+const array = [5, 6, 7]
+const emptyArray = []
 const assertEqual = function (actual, expected){
   if(actual === expected){
     console.log(`🟢🟢🟢Assertion Passed: [${actual}] === [${expected}]`)
@@ -7,8 +11,20 @@ const assertEqual = function (actual, expected){
   // console.assert(actual, expected, {actual, expected})
 }
 
+function head(input) {
+  if (input.length > 1){
+   let headNumber = input[0];
+   return headNumber
+  } else {
+    return undefined;
+}
+}
 
-assertEqual(1,1);
-assertEqual(2,3);
-assertEqual('good','good');
-assertEqual('GREAT', 'great')
+// assertEqual(1,1);
+// assertEqual(2,3);
+// assertEqual('good','good');
+// assertEqual('GREAT', 'great') 
+
+// assertEqual((head[5, 6, 7]), 5)
+
+console.log(assertEqual(head(emptyArray), 5))
---
+++ head.js
@@ -0,0 +1,8 @@
+function head(input) {
+  if (input.length > 1){
+   let headNumber = input[0];
+   return headNumber
+  } else {
+    return undefined;
+}
+}
